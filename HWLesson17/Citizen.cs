369a080e624fc207bb032da47dd16a08e9375cfa
User <User@DESKTOP-0BJFLEG>
2021-01-20 20:31:50 +0200

    Add project files.

---
+++ HWLesson17/Citizen.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWLesson17
+{
+    class Citizen
+    {
+        public string name;
+        public Citizen[] children;
+        public readonly int fatherID;
+        public readonly int id;
+        public static int numberOfCurrentCitizens;
+        public const int maximumNumberOfCitizens=10000;
+
+        public Citizen(string name, int fatherID)
+        {
+            this.name = name;
+            this.fatherID = fatherID;
+        }
+
+        public static void PrintNumberOfCitizens()
+        {
+            Console.WriteLine($"Number of current citizens: {numberOfCurrentCitizens}");
+        }
+
+        public static bool ReachedHalfOfMaximumSize() 
+        {
+            if (numberOfCurrentCitizens > maximumNumberOfCitizens)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine($"Id: {this.id}");
+        }
+
+        public void PrintGapBetweenMyIDAndFather()
+        {
+            Console.WriteLine($"Gap between my ID and father{ this.id - this.fatherID}");
+        }
+/*
+        public void SetChildren(Citizen[] children)
+        {
+            this.children = children;
+            foreach (var item in children)
+            {
+                if (item.fatherID != this.id)
+                {
+                    throw new ArgumentException("father id does not match child");
+                }
+            }
+        }
+*/
+        public void SetChildren(Citizen[] children)
+        {
+
+            for (int i = 0; i < children.Length; i++)
+            {
+                children[i] = this.children[i];            
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"name: {this.name}, ID: {this.id}, father ID: {this.fatherID}, amount of children: {this.children.Length}";
+        }
+    }
+}
---
+++ HWLesson17/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWLesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Citizen pol = new Citizen("Pol", 1);
+            Citizen sam = new Citizen("Sam", 2);
+
+            Citizen[] children =
+            {
+                new Citizen("Klara", 1),
+                new Citizen("Sara", 1),
+                new Citizen("Rima", 1)
+            };
+
+            pol.SetChildren(children);
+
+        }
+
+        public static bool CheckValidity(Citizen x) 
+        {
+            if (HasChildren(x))
+            {
+                foreach (var item in x.children)
+                {
+                    if (item.fatherID != x.id)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool HasChildren(Citizen x)
+        {
+            if (x.children.Length != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
